befd17479832f24953d31719ca258b61c05b96fc
Guilherme <guiguerreiro10@hotmail.com>
2020-01-30 00:04:27 +0000

    Connection to database established.

---
+++ conn.py
@@ -0,0 +1,38 @@
+import pyodbc
+
+
+def read(conn):
+    cursor = conn.cursor()
+    cursor.execute("select * from Services")
+    for row in cursor:
+        print(row)
+
+
+def write(conn):
+    cursor = conn.cursor()
+    cursor.execute(
+        "insert into Services(Service, Password) values(?,?);", ("Facebook", "1234456")
+    )
+    conn.commit()
+
+
+Driver = "ODBC Driver 17 for SQL Server"
+Server = r"DESKTOP-QJJ4U09\SQLEXPRESS"
+Database = "PassMng"
+
+try:
+    conn = pyodbc.connect(
+        "DRIVER="
+        + Driver
+        + ";SERVER="
+        + Server
+        + ";DATABASE="
+        + Database
+        + ";TRUSTED_CONNECTION=YES;"
+    )
+
+    print("Success! Connected to " + Database + " database.")
+except Exception as error:
+    print("Connection failed..\n")
+    print(error)
+
